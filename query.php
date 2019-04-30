2b4bd7996044ea1ac2f89ed524d90a976865a441
puripong1 <theoldman.pea@gmail.com>
2019-04-30 11:58:29 +0700

    Update query.php

--- query.php
+++ query.php
@@ -1,3 +1,11 @@
 <?php 
-require('');
+require('connect_db.php');
+
+$office = $_POST["office"];
+$keyword = $_POST["keyword"];
+
+$sql_text = "SELECT * FROM tbl_equipment WHERE office_id = '$office' AND (equip_id LIKE '%$keyword%' OR equip_name LIKE '%$keyword%')";
+$query = mysqli_query($conn,$sql_text);
+$data_return = mysqli_fetch_assoc($query);
+echo json_encode($data_return);
 ?>
